36358322261f6f22c2c1cb186fbc9c6f07b251cc
Robert Killion <robertfkillion@gmail.com>
2021-05-06 23:37:50 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {Dylan: "Idiot Wind"};
 function updatePlaylist(playlist,artistName,songTitle) {
-  playlist.artistName = songTitle;
+  playlist[artistName] = songTitle;
   return playlist;
 }
